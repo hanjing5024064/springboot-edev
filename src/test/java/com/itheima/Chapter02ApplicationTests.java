87f4f4a3886a8bc90116ac8c4d6c1c31e24d3f1e
zhangjun <5024064@qq.com>
2021-04-27 21:49:07 +0800

    # 随机值设置以及参数间引用配置

--- src/test/java/com/itheima/Chapter02ApplicationTests.java
+++ src/test/java/com/itheima/Chapter02ApplicationTests.java
@@ -6,6 +6,7 @@ import com.itheima.domain.Student;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -40,6 +41,13 @@ public class Chapter02ApplicationTests {
     @Test
     public void iocTest() {
         System.out.println(applicationContext.containsBean("myService"));
+    }
+
+    @Value("${tom.description}")
+    private String description;
+    @Test
+    public void placeholderTest() {
+        System.out.println(description);
     }
 
 }
