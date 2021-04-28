305c260b39603195e88521ba8292c47c9ed6ad25
zhangjun <5024064@qq.com>
2021-04-28 20:45:04 +0800

    打包war, 不使用spring内嵌tomcat 1. 声明war打包方式 2.声明使用外部tomcat 3.程序主类继承SpringBootServletInitializer，并重写configure()方法， 利用Spring框架的Servlet3.0支持，允许应用程序在Servlet容器启动时进行配置。         return builder.sources(ManualChapter01Application.class);//source方法第一个参数必须是主程序启动类
    maven package
    ./target目录生成 *.war

--- src/main/java/com/itheima/ManualChapter01Application.java
+++ src/main/java/com/itheima/ManualChapter01Application.java
@@ -2,13 +2,19 @@ package com.itheima;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 //@ImportResource("classpath:beans.xml") // 加载自定义XML配置文件位置
 @ServletComponentScan  // 开启基于注解方式的Servlet组件扫描支持
 @SpringBootApplication
-public class ManualChapter01Application {
+public class ManualChapter01Application extends SpringBootServletInitializer {
+    // 3、程序主类继承SpringBootServletInitializer，并重写configure()方法
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ManualChapter01Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ManualChapter01Application.class, args);
