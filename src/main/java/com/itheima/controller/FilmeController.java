2370a2d65e76c80dd013e9aba6de573d19b7257a
zhangjun <5024064@qq.com>
2021-04-29 22:10:32 +0800

    自定义用户访问控制 SecurityConfig（extends WebSecurityConfigurerAdapter）配置类中，重写    protected void configure(HttpSecurity http) throws Exception {
    http://localhost:8081/
    可以打开首页，当查看子页面，
    http://localhost:8081/detail/common/1
    进入登录页面：
    http://localhost:8081/login
    
    shitou/123456登录后，可以访问common/，访问vip/发生403错误

---
+++ src/main/java/com/itheima/controller/FilmeController.java
@@ -0,0 +1,21 @@
+package com.itheima.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @Classname FilmeController
+ * @Description TODO
+ * @Date 2019-3-5 13:45
+ * @Created by CrazyStone
+ */
+@Controller
+public class FilmeController {
+    //  影片详情页
+    @GetMapping("/detail/{type}/{path}")
+    public String toDetail(@PathVariable("type")String type, @PathVariable("path")String path) {
+        return "detail/"+type+"/"+path;
+    }
+}
+
