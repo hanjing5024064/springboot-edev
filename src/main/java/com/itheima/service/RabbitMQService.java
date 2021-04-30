12385f1dc8d8b7f72cfff7d06fc1aa80ed3ba074
zhangjun <5024064@qq.com>
2021-04-30 11:11:32 +0800

    使用基于注解的方式实现消息服务     @RabbitListener(bindings =@QueueBinding(value =@Queue("fanout_queue_sms"),exchange =@Exchange(value = "fanout_exchange",type = "fanout")))

--- src/main/java/com/itheima/service/RabbitMQService.java
+++ src/main/java/com/itheima/service/RabbitMQService.java
@@ -1,6 +1,10 @@
 package com.itheima.service;
 
+import com.itheima.domain.User;
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
@@ -12,24 +16,41 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class RabbitMQService {
+//    /**
+//     * Publish/Subscribe工作模式接收，处理邮件业务
+//     * @param message
+//     */
+//    @RabbitListener(queues = "fanout_queue_email")//也可以使用RabbitTemplate的receiveSendAndConvert()
+//    public void psubConsumerEmail(Message message) {
+//        byte[] body = message.getBody();
+//        String s = new String(body);
+//        System.out.println("邮件业务接收到消息： "+s);
+//    }
+//    /**
+//     * Publish/Subscribe工作模式接收，处理短信业务
+//     * @param message
+//     */
+//    @RabbitListener(queues = "fanout_queue_sms")
+//    public void psubConsumerSms(Message message) {
+//        byte[] body = message.getBody();
+//        String s = new String(body);
+//        System.out.println("短信业务接收到消息： "+s);
+//    }
     /**
-     * Publish/Subscribe工作模式接收，处理邮件业务
-     * @param message
+     *  **使用基于注解的方式实现消息服务
+     * 1.1、Publish/Subscribe工作模式接收，处理邮件业务
+     * @param user
      */
-    @RabbitListener(queues = "fanout_queue_email")//也可以使用RabbitTemplate的receiveSendAndConvert()
-    public void psubConsumerEmail(Message message) {
-        byte[] body = message.getBody();
-        String s = new String(body);
-        System.out.println("邮件业务接收到消息： "+s);
+    @RabbitListener(bindings =@QueueBinding(value =@Queue("fanout_queue_email"), exchange =@Exchange(value = "fanout_exchange",type = "fanout")))
+    public void psubConsumerEmailAno(User user) {
+        System.out.println("邮件业务接收到消息（注解方式）： "+user);
     }
     /**
-     * Publish/Subscribe工作模式接收，处理短信业务
-     * @param message
+     * 1.2、Publish/Subscribe工作模式接收，处理短信业务
+     * @param user
      */
-    @RabbitListener(queues = "fanout_queue_sms")
-    public void psubConsumerSms(Message message) {
-        byte[] body = message.getBody();
-        String s = new String(body);
-        System.out.println("短信业务接收到消息： "+s);
+    @RabbitListener(bindings =@QueueBinding(value =@Queue("fanout_queue_sms"),exchange =@Exchange(value = "fanout_exchange",type = "fanout")))
+    public void psubConsumerSmsAno(User user) {
+        System.out.println("短信业务接收到消息（注解方式）： "+user);
     }
 }
