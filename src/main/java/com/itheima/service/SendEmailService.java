93c5a47e77e5b80fcbcf6e812797171aa84119dd
zhangjun <5024064@qq.com>
2021-05-01 14:24:24 +0800

    发送邮件-图文邮件

--- src/main/java/com/itheima/service/SendEmailService.java
+++ src/main/java/com/itheima/service/SendEmailService.java
@@ -2,10 +2,16 @@ package com.itheima.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 /**
  * @Classname SendEmailService
@@ -39,6 +45,41 @@ public class SendEmailService {
             System.out.println("纯文本邮件发送成功");
         } catch (MailException e) {
             System.out.println("纯文本邮件发送失败 "+e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 发送复杂邮件（包括静态资源和附件）
+     * @param to           收件人地址
+     * @param subject      邮件标题
+     * @param text         邮件内容
+     * @param filePath     附件地址
+     * @param rscId        静态资源唯一标识
+     * @param rscPath      静态资源地址
+     */
+    public void sendComplexEmail(String to,String subject,String text,String filePath,String rscId,String rscPath){
+        // 定制复杂邮件信息MimeMessage
+        MimeMessage message = mailSender.createMimeMessage();
+        try {
+            // 使用MimeMessageHelper帮助类，并设置multipart多部件使用为true
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text, true);
+            // 设置邮件静态资源
+            FileSystemResource res = new FileSystemResource(new File(rscPath));
+            helper.addInline(rscId, res);
+            // 设置邮件附件
+            FileSystemResource file = new FileSystemResource(new File(filePath));
+            String fileName = filePath.substring(filePath.lastIndexOf(File.separator));
+            helper.addAttachment(fileName, file);
+            // 发送邮件
+            mailSender.send(message);
+            System.out.println("复杂邮件发送成功");
+        } catch (MessagingException e) {
+            System.out.println("复杂邮件发送失败 "+e.getMessage());
             e.printStackTrace();
         }
     }
--- src/test/java/com/itheima/Chapter09ApplicationTests.java
+++ src/test/java/com/itheima/Chapter09ApplicationTests.java
@@ -21,4 +21,23 @@ public class Chapter09ApplicationTests {
         // 发送简单邮件
         sendEmailService.sendSimpleEmail(to,subject,text);
     }
+
+    @Test
+    public void sendComplexEmailTest() {
+        String to="2127269781@qq.com";
+        String subject="【复杂邮件】标题";
+        // 定义邮件内容
+        StringBuilder text = new StringBuilder();
+        text.append("<html><head></head>");
+        text.append("<body><h1>祝大家元旦快乐！</h1>");
+        // cid为固定写法，rscId指定一个唯一标识
+        String rscId = "img001";
+        text.append("<img src='cid:" +rscId+"'/></body>");
+        text.append("</html>");
+        // 指定静态资源文件和附件路径
+        String rscPath="\\email\\newyear.jpg";
+        String filePath="\\email\\元旦放假注意事项.docx";
+        // 发送复杂邮件
+        sendEmailService.sendComplexEmail(to,subject,text.toString(),filePath,rscId,rscPath);
+    }
 }
